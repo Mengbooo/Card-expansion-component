b2dc2b72c0573e0892ede78951a8067a16817d78
QMengbooo <Bo156431362@outlook.com>
2025-02-21 23:17:12 +0800

    change css to scss / keep 1 vue file and delete css file

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,75 @@
 
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@font-face {
+  font-family: 'Ubuntu';
+  src: url('../font/Ubuntu-Bold.ttf');
+  font-weight: normal;
+  font-style: normal;
+};
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Ubuntu", sans-serif;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #0f0f0f;
+}
+
+.container {
+  width: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container .card {
+  position: absolute;
+  width: 350px;
+  height: 520px;
+  background-color: #5e5cfc;
+  color: rgba(0, 0, 0, 0);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: rgba(0, 0, 0, 0);
+  font-size: 8em;
+  font-weight: 700;
+  border: 10px solid rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
+  cursor: pointer;
+  transition: 0.52s;
+  transform-origin: 50% 100%;
+  filter: hue-rotate(calc(var(--i) * 30deg));
+  box-shadow: 0 15px 50px rgba(0, 0, 0, 0.1);
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+.container:hover .card {
+  transform: rotate(calc(var(--i) * 8deg))
+    translate(calc(var(--i) * 140px), -100px);
+  color: rgba(0, 0, 0, 0.25);
+  box-shadow: 0 15px 50px rgba(0, 0, 0, 0.25);
+}
+
+.container:active .card {
+  background-color: #333;
+}
+
+.container .card:active {
+  translate: calc(var(--i) * 20px) -50px;
+  z-index: 1;
+  background-color: #5e5cfc;
+}
+
+</style>
